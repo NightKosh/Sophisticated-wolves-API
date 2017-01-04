c5f74defc79350996982caf9118befabd49518d2
NightKosh <nightkosh@gmail.com>
2017-01-05 02:46:57 +0300

    Update for Minecraft 1.11

--- sophisticated_wolves/api/ModInfo.java
+++ sophisticated_wolves/api/ModInfo.java
@@ -8,8 +8,8 @@ package sophisticated_wolves.api;
  */
 public class ModInfo {
 
-    public static final String ID = "SophisticatedWolves";
+    public static final String ID = "sophisticatedwolves";
     public static final String NAME = "SophisticatedWolves";
-    public static final String VERSION = "3.6.1";
+    public static final String VERSION = "3.6.4";
     public static final String API_VERSION = "1.1.0";
 }
