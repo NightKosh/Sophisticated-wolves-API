6c3c88167c1602e71c50adc6f297d2088311fe4a
NightKosh <nightkosh@gmail.com>
2018-08-11 19:03:26 +0300

    Change mod & api version

--- sophisticated_wolves/api/ModInfo.java
+++ sophisticated_wolves/api/ModInfo.java
@@ -10,6 +10,6 @@ public class ModInfo {
 
     public static final String ID = "sophisticatedwolves";
     public static final String NAME = "SophisticatedWolves";
-    public static final String VERSION = "3.13.3";
-    public static final String API_VERSION = "1.3.0";
+    public static final String VERSION = "3.13.4";
+    public static final String API_VERSION = "1.3.1";
 }
