4e9b75b89d0fcb0d2e109ad6ba4819c9f245ae0c
NightKosh <nightkosh@gmail.com>
2019-05-19 15:09:25 +0300

    mod version

--- sophisticated_wolves/api/ModInfo.java
+++ sophisticated_wolves/api/ModInfo.java
@@ -10,6 +10,6 @@ public class ModInfo {
 
     public static final String ID = "sophisticatedwolves";
     public static final String NAME = "SophisticatedWolves";
-    public static final String VERSION = "3.13.7";
+    public static final String VERSION = "3.13.8";
     public static final String API_VERSION = "1.4.0";
 }
